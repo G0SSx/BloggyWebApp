b215b3ebcd9b7ba65c057b77e354b387dcc17f38
Ihor <igormochalov1881@gmail.com>
2023-08-04 13:01:07 +0300

    Init
    asp.net core MVC web app with individual accounts

---
+++ BloggyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BloggyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
